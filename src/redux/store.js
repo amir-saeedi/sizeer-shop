180311ec6ec599cdf9f7afb3cfd0bdf038f8ff0d
amir saeedi <89999824+amir-saeedi@users.noreply.github.com>
2023-11-10 01:41:14 +0330

    remove trash files

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,10 @@
 
 import { createStore } from "redux";
 import { combineReducers } from 'redux';
-import birds from './Features/counter/counterSlice';
 import carts from './cart/carts';
 import like from './like/like';
 
 const states = {
-    birds,
     carts,
     like
 }
